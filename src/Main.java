5e5dc17908a5bceea311d79aa7d2b3a5556e992c
Winston Parris <anthonyparris16@gmail.com>
2018-01-12 01:17:44 -0600

    generating random absent count list from user then outputting list

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner keyb = new Scanner(System.in);
+        System.out.printf("give me a name ");
         String msg = keyb.next();
         System.out.println("Hello " + msg + " Welcome to the Attendance App");
+        int absCount = msg.length();
+
+        ArrayList <Integer> absences = new ArrayList<Integer>();
+        Random rand  = new Random();
+        for(int i=0; i < absCount; i++ ){
+            int temp = rand.nextInt(11);
+             absences.add(temp);
+
+        }
+        System.out.println("The elements are " + absences);
+
+
     }
 }
