1c3d6375de7e481a9158291b3d0e4dcf5cd1eafe
Winston Parris <anthonyparris16@gmail.com>
2018-01-12 12:56:43 -0600

    count and output the nubmer of students with perfect attendance

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner keyb = new Scanner(System.in);
-        System.out.printf("give me a name ");
+        System.out.printf("give me a name: ");
         String msg = keyb.next();
         System.out.println("Hello " + msg + " Welcome to the Attendance App");
         int absCount = msg.length();
 
         ArrayList <Integer> absences = new ArrayList<Integer>();
         Random rand  = new Random();
+        int perfAttendanceCount = 0;
         for(int i=0; i < absCount; i++ ){
             int temp = rand.nextInt(11);
              absences.add(temp);
-
+            if(temp==0){
+                perfAttendanceCount++;
+            }
         }
-        System.out.println("The elements are " + absences);
 
+
+        System.out.println("The elements are " + absences);
+        System.out.println(perfAttendanceCount + " student(s) had perfect attendance");
 
     }
 }
