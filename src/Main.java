a8f12648391aa1c0bddddfc698bfcee4d78a96c6
Winston Parris <anthonyparris16@gmail.com>
2018-01-22 18:44:39 -0600

    compartmentalized previous steps of the program into individual functions

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,55 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    final static Scanner keyb = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner keyb = new Scanner(System.in);
+
+
+        ArrayList<Integer> attendanceRecord = calcAbsences(welcomeInput());
+        System.out.println("This many student(s) had perfect Attendance: " + calcPerfAttendance(attendanceRecord));
+        System.out.println("The average absent rate is: " + calcAverageAbsence(attendanceRecord));
+        System.out.println(attendanceRecord);
+
+
+    }
+
+    public static int welcomeInput() {
         System.out.printf("give me a name: ");
         String msg = keyb.next();
         System.out.println("Hello " + msg + " Welcome to the Attendance App");
-        int absCount = msg.length();
+        return msg.length();
+    }
 
-        ArrayList <Integer> absences = new ArrayList<Integer>();
-        Random rand  = new Random();
+    public static ArrayList<Integer> calcAbsences(int absencesCnt) {
+        ArrayList<Integer> absences = new ArrayList<Integer>();
+        Random rand = new Random();
         int perfAttendanceCount = 0;
-        for(int i=0; i < absCount; i++ ){
+        for (int i = 0; i < absencesCnt; i++) {
             int temp = rand.nextInt(11);
-             absences.add(temp);
-            if(temp==0){
+            absences.add(temp);
+
+        }
+        return absences;
+    }
+
+    public static int calcPerfAttendance(ArrayList<Integer> arr) {
+        int perfAttendanceCount = 0;
+        for (int elem : arr) {
+            if (elem == 0) {
                 perfAttendanceCount++;
             }
 
         }
-
-
-        System.out.println("The elements are " + absences);
-        System.out.println(perfAttendanceCount + " student(s) had perfect attendance");
-        System.out.println(calcAverageAbsence(absences));
-
+        return perfAttendanceCount;
     }
-     public static int calcAverageAbsence (ArrayList<Integer> arr){
+
+    public static int calcAverageAbsence(ArrayList<Integer> arr) {
         int avgAbs = 0;
-        return avgAbs;
+        int absTotal = 0;
+        for (int elem : arr) {
+            absTotal += elem;
+        }
+        return absTotal / arr.size();
     }
 }
